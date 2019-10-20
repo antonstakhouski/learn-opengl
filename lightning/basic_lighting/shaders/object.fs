de5376f25dfa0715fb22389b3fde4ef754fb5955
Anton Stakhouski <antonstakhouski@gmail.com>
2019-10-20 15:54:18 +0300

    add basic lighting code

---
+++ lightning/basic_lighting/shaders/object.fs
@@ -0,0 +1,31 @@
+#version 330 core
+
+in vec3 normal;
+in vec3 fragPos;
+
+out vec4 fragColor;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor; 
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+    float ambientStength = 0.1f;
+    vec3 ambient = ambientStength * lightColor;
+
+    vec3 norm = normalize(normal);
+    vec3 lightDir = normalize(lightPos - fragPos);
+    float diff = max(dot(norm, lightDir), 0.0f);
+    vec3 diffuse = diff * lightColor;
+
+    float specularStrength = 0.5f;
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32.0f);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    fragColor = vec4(result, 1.0f);
+}
